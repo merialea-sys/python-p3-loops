daeefe4ea6f9e1aecc01f731b5e5ec0940c1e984
merialea <merialea5@gmail.com>
2025-11-13 15:16:40 +0300

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
+    squared_ints = []
+    for num in int_list: 
+        squared_ints.append(num * num)
+
+    return squared_ints
+
     pass
 
 def fizzbuzz():
-    # code goes here!
+    for num in range(1, 101):
+        if num % 15 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else :
+            print(num)
     pass
